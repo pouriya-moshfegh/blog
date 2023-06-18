05142454c7b3cc1f27629b80d94abfe8b8cc0976
pouriya moshfegh <pouriya.moshfeghs@gmail.com>
2023-06-18 22:23:47 +0330

    photo fixed

--- src/app/components/MyProfilePic.tsx
+++ src/app/components/MyProfilePic.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import React from "react";
+import photo from "../../../public/image/my-photo.jpg"
 
 export default function MyProfilePic() {
   return (
     <section className="w-full mx-auto ">
       <Image
         className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
-        src="/image/my-photo.jpg  "
+        src={photo}
         width={200}
         height={200}
         alt="my photo"
